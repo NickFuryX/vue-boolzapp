dfee3dd1eafef813f7e8846a9365146bbb94d3a7
NickfuryX <nosinicolo@gmail.com>
2022-04-26 15:49:26 +0200

    check 1

---
+++ js/script.js
@@ -0,0 +1,9 @@
+// Utility
+function getRandomInt(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return parseInt(Math.floor(Math.random() * (max - min) + min));
+  }
+
+
+//-------------------------------------------------------------------------------------------//
